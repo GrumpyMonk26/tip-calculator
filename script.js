22f80d3e3d0dd0f6ed454f702693b39328a8f8e8
Jason Evans <evans.jason@hotmail.co.uk>
2023-05-11 15:12:23 +0100

    fixed error with when no number entered

--- script.js
+++ script.js
@@ -2,32 +2,7 @@ const buttonEl = document.getElementById('btn');
 const billAmountEl = document.getElementById('bill-amount');
 const tipAmountEl = document.getElementById('tip-percentage');
 const totalEl = document.getElementById('total');
-const errorEl = document.querySelector('.total');
-
-// function calculateBill() {
-//   const billAmount = Number(billAmountEl.value);
-//   const tipAmount = Number(tipAmountEl.value);
-
-//   if (!billAmount || !tipAmount) {
-//     errorEl.textContent = 'There has been an error, enter bill and tip amount!';
-//     errorEl.style.color = 'red';
-//     setTimeout(() => {
-//       errorEl.textContent = '';
-//     }, 2000);
-//     return;
-//   } else {
-//     const bill = (tipAmount * billAmount) / 100 + billAmount;
-//     totalEl.textContent = `£ ${bill}`;
-//     errorEl.textContent = 'Total';
-
-//     // Clear inputs after 3 seconds
-//     setTimeout(() => {
-//       billAmountEl.value = '';
-//       tipAmountEl.value = '';
-//       totalEl.textContent = '';
-//     }, 3000);
-//   }
-// }
+// const errorEl = document.querySelector('.total');
 
 function calculateBill() {
   const billAmount = Number(billAmountEl.value);
